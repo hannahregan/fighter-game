11d704e948c6d6e4e0d486640fe077c6abdac5a4
MackFree <michaelfreeman100@gmail.com>
2019-05-06 22:23:40 +1200

    Punching getting there

--- src/com/company/GameScreen.java
+++ src/com/company/GameScreen.java
@@ -1,12 +1,21 @@
 package com.company;
 
-import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.*;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
+import java.awt.*;
+
 public class GameScreen extends BasicGameState {
+
+    public Player player;
+    public Image playerImg;
+
+    //placeholder
+    public Rectangle ground;
+
     public GameScreen(int gameState){
 
     }
@@ -17,17 +26,53 @@ public class GameScreen extends BasicGameState {
     }
 
     @Override
-    public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+        player = new Player("0");
+        playerImg = new Image("/res/gameScreen/playerself.png");
 
+        //placeholder for ground
+        ground = new Rectangle(0, 540, 880,40);
     }
 
     @Override
-    public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
-
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+        playerImg.draw(player.getXpos(), player.getYpos());
     }
 
     @Override
-    public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
+    public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
 
+        //getting the input from the keyboard
+        Input input = gc.getInput();
+
+        //moving the player
+        if(input.isKeyDown(input.KEY_LEFT)) {
+            player.move_left(delta);
+        }else if(input.isKeyDown(input.KEY_RIGHT)){
+            player.move_right(delta);
+        }
+
+        //makes the player jump
+        if(input.isKeyPressed(input.KEY_UP) && player.getOnground()){
+            player.jump(delta);
+        }
+
+        //punch
+        if(input.isKeyPressed(input.KEY_Q)){
+            player.punch();
+        }
+
+        //gravity
+        if(player.getJumping() == false && player.getPlayerHitBox().intersects(ground) == false){
+            player.fall(delta);
+            player.setOnground(false);
+        }else{
+            player.setOnground(true);
+        }
+
+
+
+        //updating player
+        player.update(delta);
     }
 }
---
+++ src/com/company/Player.java
@@ -0,0 +1,210 @@
+package com.company;
+
+import java.awt.*;
+
+public class Player {
+    //the player played by the person using this client
+    public int xpos, ypos, jumpTimer, health, abilityTimer, punchTimer;
+    public Rectangle playerHitBox, punchHitBox;
+    public Boolean jumping, onground, punching;
+
+    public Player(String type){
+        //positionss
+        this.xpos = 100;
+        this.ypos = 100;
+
+        //timers
+        this.jumpTimer = 0;
+        this.punchTimer = 0;
+
+
+        //jumping shite
+        this.setPunching(false);
+        this.setOnground(false);
+        this.setJumping(false);
+
+        //hitboxes
+        playerHitBox = new Rectangle(this.xpos, this.ypos, 70, 100);
+        punchHitBox = new Rectangle(this.xpos, this.ypos, 0, 0);
+
+        //health ect
+        this.health = 100;
+        this.abilityTimer = 50; //halfway
+
+        //type passed from select player screen
+        switch(type){
+            case "1":
+                //statements
+                break;
+            case "2":
+                //statements
+                break;
+            case "3":
+
+                break;
+            case "4":
+
+                break;
+            case "5":
+
+                break;
+            case "6":
+
+                break;
+            default:
+                break;
+
+        }
+        //switch ends
+
+    }
+
+    public void update(int delta){
+        //keeps the hitbox at the player && may soon do other stuff
+        playerHitBox.setLocation(this.xpos, this.ypos);
+
+        //jumping
+        if(this.jumpTimer > 0){
+            this.ypos -= delta;
+            this.setOnground(false);
+            this.jumpTimer -= 1;
+        }else if(this.jumpTimer == 0){
+            this.setJumping(false);
+        }
+
+        if(this.getPunchTimer() > 50){
+            //punching out
+            pun
+            this.setPunchTimer(this.getPunchTimer() - 1);
+        }else if(this.getPunchTimer() > 1){
+            //punching in
+
+            this.setPunchTimer(this.getPunchTimer() - 1);
+        }else{
+            this.setPunching(false);
+        }
+
+    }
+
+    public int jump(int delta){
+        this.setJumping(true);
+        //returns jumping timers
+        this.jumpTimer = 200;
+        return this.jumpTimer;
+    }
+
+    public void punch(){
+        //punching
+        this.setPunching(true);
+        this.setPunchTimer(100);
+    }
+
+    public int move_left(int delta){
+        //takes the delta of the game and moves the player left by that much
+        this.xpos -= delta;
+        //returns the xpos for checking collisions ect
+        return xpos;
+    }
+
+    public int move_right(int delta){
+        //takes delta and move the player right by that much
+        this.xpos += delta;
+        //returns xpos for collision detection
+        return xpos;
+    }
+
+    public int fall(int delta){
+        //makes the player fall, only called if not intersecting
+        this.ypos += delta;
+        return this.ypos;
+    }
+
+    public int getXpos() {
+        return xpos;
+    }
+
+    public Rectangle getPunchHitBox() {
+        return punchHitBox;
+    }
+
+    public void setPunchHitBox(Rectangle punchHitBox) {
+        this.punchHitBox = punchHitBox;
+    }
+
+    public Boolean getPunching() {
+        return punching;
+    }
+
+    public int getPunchTimer() {
+        return punchTimer;
+    }
+
+    public void setPunchTimer(int punchTimer) {
+        this.punchTimer = punchTimer;
+    }
+
+    public void setPunching(Boolean punching) {
+        this.punching = punching;
+    }
+
+    public void setXpos(int xpos) {
+        this.xpos = xpos;
+    }
+
+    public int getYpos() {
+        return ypos;
+    }
+
+    public int getJumpTimer() {
+        return jumpTimer;
+    }
+
+    public void setJumpTimer(int jumpTimer) {
+        this.jumpTimer = jumpTimer;
+    }
+
+    public Boolean getOnground() {
+        return onground;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getAbilityTimer() {
+        return abilityTimer;
+    }
+
+    public void setAbilityTimer(int abilityTimer) {
+        this.abilityTimer = abilityTimer;
+    }
+
+    public void setOnground(Boolean onground) {
+        this.onground = onground;
+    }
+
+    public void setYpos(int ypos) {
+        this.ypos = ypos;
+    }
+
+    public Rectangle getPlayerHitBox() {
+        return playerHitBox;
+    }
+
+    public void setPlayerHitBox(Rectangle playerHitBox) {
+        this.playerHitBox = playerHitBox;
+    }
+
+    public Boolean getJumping() {
+        return jumping;
+    }
+
+    public void setJumping(Boolean jumping) {
+        this.jumping = jumping;
+    }
+
+}
