222ae2bc0d5a4b2c26d10be5b24d414022d09955
MackFree <michaelfreeman100@gmail.com>
2019-05-06 21:22:49 +1200

    Start Screen Implemented

--- src/com/company/StartScreen.java
+++ src/com/company/StartScreen.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Input;
-import org.newdawn.slick.SlickException;
+import org.newdawn.slick.*;
 import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -12,7 +9,7 @@ import static org.newdawn.slick.Input.KEY_ENTER;
 
 public class StartScreen extends BasicGameState {
 
-    public Circle circle;
+    public Image backgroundStart, logoStart, startBtn, quitBtn, optionsBtn;
 
     public StartScreen(int gameState) {
 
@@ -27,21 +24,58 @@ public class StartScreen extends BasicGameState {
     @Override
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
         //setup
-        circle = new Circle(100, 100, 50);
+
+        //background and logo
+        backgroundStart = new Image("res/startScreen/backgroundstart.png");
+        logoStart = new Image("res/startScreen/logostart.png");
+
+        //buttons
+        startBtn = new Image("res/startScreen/startbtn.png");
+        quitBtn = new Image("res/startScreen/quitbtn.png");
+        optionsBtn = new Image("res/startScreen/optionsbtn.png");
     }
 
     @Override
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
-        //draw
-        g.draw(circle);
+        //draw//images 580 wide (buttons) screen(880, 600)
+
+        //drawring images
+        backgroundStart.draw(0,0);
+
+        logoStart.draw(150, 60);
+
+        startBtn.draw(150, 260);
+        optionsBtn.draw(150, 360);
+        quitBtn.draw(150, 460);
     }
 
     @Override
     public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
         //stuff happends
         Input input = gc.getInput();
-        if(input.isKeyPressed(input.KEY_ENTER)){
-            sbg.enterState(1);
+        int xpos = input.getMouseX();
+        int ypos = input.getMouseY();
+        //now we check if it is clicked
+        if(input.isMouseButtonDown(0)){
+            //left mouse button
+
+            //now check if within each button bounds
+            if(xpos > 150 && xpos < 730 && ypos > 260 && ypos < 330){
+                //start
+                sbg.enterState(1);
+            }
+
+            if(xpos > 150 && xpos < 730 && ypos > 360 && ypos < 430){
+                //options
+                System.out.println("Not implemeented");
+            }
+
+            if(xpos > 150 && xpos < 730 && ypos > 460 && ypos < 530){
+                //quit
+                gc.exit();
+            }
         }
+
+
     }
 }
